aa69f83298c25d3f88ad91e19ef03435b92171f7
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 14:49:17 +0100

    Add tests that certain pieces cannot leave the board

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -15,40 +15,89 @@ export default class Bishop extends Piece {
         let forwardsDiagonalMoves = [];
         let backwardsDiagonalMoves = [];
         let DStart = Square.at(location.row + 1, location.col + 1);
+        let finalMoves = [];
 
-        // Forwards diagonal
-        while (DStart.row < 8 && DStart.col < 8) 
-        {
+        // North East
+        while (DStart.row < 8 && DStart.col < 8) {
             forwardsDiagonalMoves.push(Square.at(DStart.row, DStart.col));
             DStart.row += 1;
             DStart.col += 1;
         }
-
+        //South West 
         DStart = Square.at(location.row - 1, location.col - 1);
-        while (DStart.row >= 0 && DStart.col >= 0) 
-        {
+        while (DStart.row >= 0 && DStart.col >= 0) {
             forwardsDiagonalMoves.push(Square.at(DStart.row, DStart.col));
             DStart.row -= 1;
             DStart.col -= 1;
         }
 
-        // Backwards diagonal
+        // Backwards diagonal - North West
         DStart = Square.at(location.row + 1, location.col - 1);
-        while (DStart.col >= 0) 
-        {
+        while (DStart.col >= 0 && DStart.row < 8) {
             backwardsDiagonalMoves.push(Square.at(DStart.row, DStart.col));
             DStart.row += 1;
             DStart.col -= 1;
         }
-
+        // Backwards diagonal - South East
         DStart = Square.at(location.row - 1, location.col + 1);
-        while (DStart.row >= 0) 
-        {
+        while (DStart.row >= 0 && DStart.col < 8) {
             backwardsDiagonalMoves.push(Square.at(DStart.row, DStart.col));
             DStart.row -= 1;
             DStart.col += 1;
         }
 
-        return [...backwardsDiagonalMoves, ...forwardsDiagonalMoves];
+        // return [...backwardsDiagonalMoves, ...forwardsDiagonalMoves];
+        backwardsDiagonalMoves = backwardsDiagonalMoves.sort((a,b) => a.row - b.row);
+        forwardsDiagonalMoves = forwardsDiagonalMoves.sort((a,b) => a.row - b.row);
+
+        let totalPossibleMovesArray = [...backwardsDiagonalMoves, ...forwardsDiagonalMoves];
+
+        let takingMoves = [];
+        let blockingPieces = [];
+
+        // Get positions of pieces (blocking pieces) that are located on the possible moves
+        for (let i = 0; i < totalPossibleMovesArray.length; i++) {
+            if (board.getPiece(totalPossibleMovesArray[i])) {
+                let blockingPiece = board.getPiece(totalPossibleMovesArray[i]);
+                let blockingPieceLocation = board.findPiece(blockingPiece);
+                if (blockingPiece.constructor.name === "King") {
+                    blockingPieces.push(blockingPieceLocation);
+                } else if (blockingPiece.player.description === sameTeam){ 
+                    blockingPieces.push(blockingPieceLocation);
+                } else if (blockingPiece.player.description === opposingTeam) { 
+                    blockingPieces.push(blockingPieceLocation);
+                    takingMoves.push(blockingPieceLocation);
+                }
+            }
+        }
+
+        function findIndex(blockingPieces, direction) {
+            if (direction === "forwards") {
+                return forwardsDiagonalMoves.findIndex(move => move.row === blockingPieces.row && move.col === blockingPieces.col);
+            } else {
+                return backwardsDiagonalMoves.findIndex(move => move.row === blockingPieces.row && move.col === blockingPieces.col);
+            }
+        }
+
+        for (let i = 0; i < blockingPieces.length; i++) {
+            if (blockingPieces[i].row > location.row && blockingPieces[i].col > location.col) {
+                forwardsDiagonalMoves.splice(findIndex(blockingPieces[i], "forwards")); 
+            } else if (blockingPieces[i].row < location.row && blockingPieces[i].col > location.col) {
+                backwardsDiagonalMoves.splice(0, findIndex(blockingPieces[i], "backwards") + 1); 
+            } else if (blockingPieces[i].row < location.row && blockingPieces[i].col < location.col) {
+                forwardsDiagonalMoves.splice(0, findIndex(blockingPieces[i], "forwards") + 1); 
+            } else if (blockingPieces[i].row > location.row && blockingPieces[i].col < location.col) {
+                backwardsDiagonalMoves.splice(findIndex(blockingPieces[i], "backwards")); 
+            }
+        }
+
+        finalMoves = [...backwardsDiagonalMoves, ...forwardsDiagonalMoves];
+
+        if (takingMoves.length > 0) {
+            for (let i of takingMoves) {
+                finalMoves.push(i)
+            }
+        }
+        return finalMoves;
     }
 }
