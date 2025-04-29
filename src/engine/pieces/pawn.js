38a04c220dfc99d65b36060f4210a9f09db45dba
Hannah Dunsmore <hannah.dunsmore@softwire.com>
2025-04-29 16:47:53 +0100

    Passed Red 8- pawn cannot move if there is a piece in front

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -12,12 +12,22 @@ export default class Pawn extends Piece {
 
         if (this.player === Player.WHITE) {
             if (location.row === 1) {
-                // return [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)];
+                console.log('running');
                 let possibleMoves = [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)];
                 let availableMoves = [];
 
+                // for (let i = 0; i < possibleMoves.length; i++) {
+                //     if (typeof board.getPiece(possibleMoves[i]) != 'object') {
+                //         availableMoves.push(possibleMoves[i]);
+                //     }
+                // }
+
                 for (let i = 0; i < possibleMoves.length; i++) {
-                    if (typeof board.getPiece(possibleMoves[i]) != 'object') {
+                    if (typeof board.getPiece(possibleMoves[0]) === 'object') {
+                        return [];
+                    } else if (typeof board.getPiece(possibleMoves[1]) === 'object') {
+                        return [];
+                    } else {
                         availableMoves.push(possibleMoves[i]);
                     }
                 }
