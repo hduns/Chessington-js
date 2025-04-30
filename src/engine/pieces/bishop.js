6fe8400b31077d527c8da75ed0a64beb76f3f5f9
Hannah Dunsmore <hannah.dunsmore@softwire.com>
2025-04-30 22:46:17 +0100

    passed Red 4 Bishops. Bishops can move diagonally. fixed

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -11,34 +11,43 @@ export default class Bishop extends Piece {
     getAvailableMoves(board) {
         let opposingTeam = this.player === Player.WHITE ? "black" : "white";
         let sameTeam = this.player === Player.WHITE ? "white" : "black";
-
         let location = board.findPiece(this);
-
-        // Forwards Diagonal 
-        let FDStart = [Square.at(location.row -2, location.col -2)];
         let forwardsDiagonalMoves = [];
-
-        let i = 0;
-        do {
-            if (FDStart[0].row != location.row) {
-                forwardsDiagonalMoves.push(Square.at(FDStart[0].row, FDStart[0].col));
-            }
-            FDStart[0].row += 1;
-            FDStart[0].col += 1;
-            i++
-        } while (i < 7)
-
-        // Backwards Diagonal
-        let BDStart = [Square.at(location.row - location.row, location.col + location.row)];
         let backwardsDiagonalMoves = [];
+        let DStart = Square.at(location.row + 1, location.col + 1);
 
-        do {
-            if (BDStart[0].row != location.row) {
-                backwardsDiagonalMoves.push(Square.at(BDStart[0].row, BDStart[0].col));
-            }
-            BDStart[0].row += 1;
-            BDStart[0].col -= 1;
-        } while (BDStart[0].col >= 0)
+        // Forwards diagonal
+        while (DStart.row < 8 && DStart.col < 8) 
+        {
+            forwardsDiagonalMoves.push(Square.at(DStart.row, DStart.col));
+            DStart.row += 1;
+            DStart.col += 1;
+        }
+
+        DStart = Square.at(location.row - 1, location.col - 1);
+        while (DStart.row >= 0 && DStart.col >= 0) 
+        {
+            forwardsDiagonalMoves.push(Square.at(DStart.row, DStart.col));
+            DStart.row -= 1;
+            DStart.col -= 1;
+        }
+
+        // Backwards diagonal
+        DStart = Square.at(location.row + 1, location.col - 1);
+        while (DStart.col >= 0) 
+        {
+            backwardsDiagonalMoves.push(Square.at(DStart.row, DStart.col));
+            DStart.row += 1;
+            DStart.col -= 1;
+        }
+
+        DStart = Square.at(location.row - 1, location.col + 1);
+        while (DStart.row >= 0) 
+        {
+            backwardsDiagonalMoves.push(Square.at(DStart.row, DStart.col));
+            DStart.row -= 1;
+            DStart.col += 1;
+        }
 
         return [...backwardsDiagonalMoves, ...forwardsDiagonalMoves];
     }
